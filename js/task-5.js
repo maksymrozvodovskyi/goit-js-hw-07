fc68a21fbd057b36880492519790ff9a272edd78
maksym rozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-02-18 14:34:26 +0200

    add task 4 and 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonChange = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const textColor = document.querySelector('.color');
+
+buttonChange.addEventListener('click', evt => {
+  const randomColor = getRandomHexColor();
+
+  body.style.backgroundColor = randomColor;
+
+  textColor.textContent = randomColor;
+});
